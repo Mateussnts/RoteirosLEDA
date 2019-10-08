0ed2d6237d3f6b05e0d1d73528f41020b041bccd
Mateus Santos <mateus.santos@ccc.ufcg.edu.br>
2019-10-07 21:18:44 -0300

    R9

--- R09-01-Rot-TabelaHash-EndAberto-environment/src/main/java/adt/hashtable/open/HashtableOpenAddressLinearProbingImpl.java
+++ R09-01-Rot-TabelaHash-EndAberto-environment/src/main/java/adt/hashtable/open/HashtableOpenAddressLinearProbingImpl.java
@@ -15,25 +15,77 @@ public class HashtableOpenAddressLinearProbingImpl<T extends Storable> extends
 
 	@Override
 	public void insert(T element) {
-		throw new UnsupportedOperationException("Not implemented yet!");
+		if(this.isFull())
+			throw new HashtableOverflowException();
+	
+		if(element != null) {
+			if(this.search(element) != null)
+				return;
+			
+			int valor = 0;
+			int hashIndex = ((HashFunctionLinearProbing<T>) hashFunction).hash(element, valor);
+			while (valor < table.length) {
+ 				if (table[hashIndex] == null || deletedElement.equals(table[hashIndex])) {
+                	table[hashIndex] = element;
+                 	super.elements++;
+                 	return;
+              	} 
+ 				else {
+                 	valor++;
+                 	hashIndex = ((HashFunctionLinearProbing<T>) hashFunction).hash(element, valor);
+                 	super.COLLISIONS++;
+              	}
+			}
+		}
 	}
 
 	@Override
 	public void remove(T element) {
-		// TODO Auto-generated method stub
-		throw new UnsupportedOperationException("Not implemented yet!");
+		if(this.isEmpty())
+			throw new HashtableOverflowException();
+
+		int hashIndex = indexOf(element);
+		if (hashIndex >= 0) {
+			table[hashIndex] = new DELETED();
+			super.elements--;
+		}
 	}
 
 	@Override
 	public T search(T element) {
-		// TODO Auto-generated method stub
-		throw new UnsupportedOperationException("Not implemented yet!");
+		if (element != null) {
+			int valor = 0;
+			int hashIndex = ((HashFunctionLinearProbing<T>) hashFunction).hash(element, valor);
+
+			while (valor < table.length) {
+				if (table[hashIndex] != null && table[hashIndex].equals(element)) {
+					return element;
+				} 
+				else {
+					valor++;
+					hashIndex = ((HashFunctionLinearProbing<T>) hashFunction).hash(element, valor);
+				}
+			}
+		}
+		return null;
 	}
 
 	@Override
 	public int indexOf(T element) {
-		// TODO Auto-generated method stub
-		throw new UnsupportedOperationException("Not implemented yet!");
+		if (this.search(element) != null) {
+			int valor = 0;
+			int hashIndex = ((HashFunctionLinearProbing<T>) hashFunction).hash(element, valor);
+			
+			while (valor < table.length) {
+				if (table[hashIndex].equals(element)) {
+					return hashIndex;
+				} 
+				else {
+					valor++;
+					hashIndex = ((HashFunctionLinearProbing<T>) hashFunction).hash(element, valor);
+				}
+			}
+		}
+		return -1;
 	}
-
 }
--- R09-01-Rot-TabelaHash-EndAberto-environment/src/main/java/adt/hashtable/open/HashtableOpenAddressQuadraticProbingImpl.java
+++ R09-01-Rot-TabelaHash-EndAberto-environment/src/main/java/adt/hashtable/open/HashtableOpenAddressQuadraticProbingImpl.java
@@ -3,11 +3,9 @@ package adt.hashtable.open;
 import adt.hashtable.hashfunction.HashFunctionClosedAddressMethod;
 import adt.hashtable.hashfunction.HashFunctionQuadraticProbing;
 
-public class HashtableOpenAddressQuadraticProbingImpl<T extends Storable>
-		extends AbstractHashtableOpenAddress<T> {
+public class HashtableOpenAddressQuadraticProbingImpl<T extends Storable> extends AbstractHashtableOpenAddress<T> {
 
-	public HashtableOpenAddressQuadraticProbingImpl(int size,
-			HashFunctionClosedAddressMethod method, int c1, int c2) {
+	public HashtableOpenAddressQuadraticProbingImpl(int size, HashFunctionClosedAddressMethod method, int c1, int c2) {
 		super(size);
 		hashFunction = new HashFunctionQuadraticProbing<T>(size, method, c1, c2);
 		this.initiateInternalTable(size);
@@ -15,25 +13,78 @@ public class HashtableOpenAddressQuadraticProbingImpl<T extends Storable>
 
 	@Override
 	public void insert(T element) {
-		// TODO Auto-generated method stub
-		throw new UnsupportedOperationException("Not implemented yet!");
+		if (this.isFull())
+			throw new HashtableOverflowException();
+
+		if (element != null) {
+			if (this.search(element) != null)
+				return;
+
+			int valor = 0;
+			int hashIndex = ((HashFunctionQuadraticProbing<T>) hashFunction).hash(element, valor);
+
+			while (valor < table.length) {
+				if (table[hashIndex] == null || deletedElement.equals(table[hashIndex])) {
+					table[hashIndex] = element;
+					super.elements++;
+					return;
+				} else {
+					valor++;
+					hashIndex = ((HashFunctionQuadraticProbing<T>) hashFunction).hash(element, valor);
+					super.COLLISIONS++;
+				}
+			}
+
+		}
 	}
 
 	@Override
 	public void remove(T element) {
-		// TODO Auto-generated method stub
-		throw new UnsupportedOperationException("Not implemented yet!");
+		if (this.isEmpty())
+			throw new HashtableOverflowException();
+
+		int hashIndex = indexOf(element);
+
+		if (hashIndex >= 0) {
+			table[hashIndex] = new DELETED();
+			super.elements--;
+		}
 	}
 
 	@Override
 	public T search(T element) {
-		// TODO Auto-generated method stub
-		throw new UnsupportedOperationException("Not implemented yet!");
+		if (element != null) {
+			int valor = 0;
+			int hashIndex = ((HashFunctionQuadraticProbing<T>) hashFunction).hash(element, valor);
+
+			while (valor < table.length) {
+				if (table[hashIndex] != null && table[hashIndex].equals(element)) {
+					return element;
+				} else {
+					valor++;
+					hashIndex = ((HashFunctionQuadraticProbing<T>) hashFunction).hash(element, valor);
+				}
+			}
+		}
+
+		return null;
 	}
 
 	@Override
 	public int indexOf(T element) {
-		// TODO Auto-generated method stub
-		throw new UnsupportedOperationException("Not implemented yet!");
+		if (this.search(element) != null) {
+			int valor = 0;
+			int hashIndex = ((HashFunctionQuadraticProbing<T>) hashFunction).hash(element, valor);
+
+			while (valor < table.length) {
+				if (table[hashIndex].equals(element)) {
+					return hashIndex;
+				} else {
+					valor++;
+					hashIndex = ((HashFunctionQuadraticProbing<T>) hashFunction).hash(element, valor);
+				}
+			}
+		}
+		return -1;
 	}
 }
