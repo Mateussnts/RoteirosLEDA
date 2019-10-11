31139c5834c8fce9b0f7e6790d025be7b6e977b3
Mateus Santos <mateus.santos@ccc.ufcg.edu.br>
2019-10-10 22:56:02 -0300

    added r10 o bruxo complet

--- R10-01-Rot-SetLinkedList-environment/src/main/java/adt/linkedList/SingleLinkedListImpl.java
+++ R10-01-Rot-SetLinkedList-environment/src/main/java/adt/linkedList/SingleLinkedListImpl.java
@@ -10,52 +10,96 @@ public class SingleLinkedListImpl<T> implements LinkedList<T> {
 
 	@Override
 	public boolean isEmpty() {
-		if(this.head.getData() == null)
+		if (this.head.data == null)
 			return true;
 		return false;
 	}
 
 	@Override
 	public int size() {
-		int numero = 0;
+		int size = 0;
 		SingleLinkedListNode<T> aux = this.head;
-		while(!aux.isNIL()) {
-			numero++;
-			aux = aux.getNext();
+		while (!aux.isNIL()) {
+			size += 1;
+			aux = aux.next;
 		}
-		return numero;
+		return size;
 	}
 
 	@Override
 	public T search(T element) {
-		// TODO Auto-generated method stub
-		throw new UnsupportedOperationException("Not implemented!");
+		if (this.head.data.equals(element)) {
+			return this.head.data;
+		} else {
+			SingleLinkedListNode<T> aux = this.head;
+			while (!aux.isNIL() && !aux.data.equals(element)) {
+				aux = aux.next;
+			}
+			if (!aux.isNIL()) {
+				return aux.data;
+			}
+		}
+		return null;
 	}
 
 	@Override
 	public void insert(T element) {
-		// TODO Auto-generated method stub
-		throw new UnsupportedOperationException("Not implemented!");
+		if (element != null) {
+			SingleLinkedListNode<T> auxHead = this.head;
+			SingleLinkedListNode<T> newHead;
+			SingleLinkedListNode<T> newNode;
+			if (this.head.isNIL()) {
+				newHead = new SingleLinkedListNode<>(element, this.head);
+				this.head = newHead;
+			} else {
+				while (!auxHead.next.isNIL()) {
+					auxHead = auxHead.next;
+				}
+				newNode = new SingleLinkedListNode<>(element, new SingleLinkedListNode<>());
+				newNode.next = auxHead.next;
+				auxHead.next = newNode;
+			}
+		}
 	}
 
 	@Override
 	public void remove(T element) {
-		// TODO Auto-generated method stub
-		throw new UnsupportedOperationException("Not implemented!");
+		if (!this.isEmpty()) {
+			if (this.head.data.equals(element)) {
+				this.head = this.head.next;
+			} else {
+				SingleLinkedListNode<T> aux = this.head;
+				SingleLinkedListNode<T> previous = aux;
+				while (!aux.isNIL() && aux.data != element) {
+					previous = aux;
+					aux = aux.next;
+				}
+				if (!aux.isNIL()) {
+					previous.next = aux.next;
+				}
+			}
+		}
 	}
 
 	@Override
 	public T[] toArray() {
-		// TODO Auto-generated method stub
-		throw new UnsupportedOperationException("Not implemented!");
+		@SuppressWarnings("unchecked")
+		T[] resultado = (T[]) new Object[this.size()];
+		SingleLinkedListNode<T> aux = this.head;
+		int count = 0;
+		while (!aux.isNIL()) {
+			resultado[count] = aux.data;
+			count += 1;
+			aux = aux.next;
+		}
+		return resultado;
 	}
 
 	public SingleLinkedListNode<T> getHead() {
-		return head;
+		return this.head;
 	}
 
 	public void setHead(SingleLinkedListNode<T> head) {
 		this.head = head;
 	}
-
 }
--- R10-01-Rot-SetLinkedList-environment/src/main/java/adt/linkedList/set/SetLinkedListImpl.java
+++ R10-01-Rot-SetLinkedList-environment/src/main/java/adt/linkedList/set/SetLinkedListImpl.java
@@ -6,26 +6,66 @@ public class SetLinkedListImpl<T> extends SingleLinkedListImpl<T> implements Set
 
 	@Override
 	public void removeDuplicates() {
-		//TODO Implement your code here
-		throw new UnsupportedOperationException("Not implemented yet!");
+		T[] aux = this.toArray();
+		int i = aux.length - 1;
+		int j = i - 1;
+		while (i > 0) {
+			while (j > 0) {
+				if (aux[i].equals(aux[j])) {
+					this.remove(aux[i]);
+					break;
+				}
+				j--;
+			}
+			i--;
+			j = i - 1;
+		}
 	}
 
 	@Override
 	public SetLinkedList<T> union(SetLinkedList<T> otherSet) {
-		//TODO Implement your code here
-		throw new UnsupportedOperationException("Not implemented yet!");
+		SetLinkedListImpl<T> aux = new SetLinkedListImpl<>();
+		T[] a = this.toArray();
+		for (T element : a) {
+			aux.insert(element);
+		}
+
+		T[] b = otherSet.toArray();
+		for (T element : b) {
+			if (aux.search(element) == null) {
+				aux.insert(element);
+			}
+		}
+		return aux;
 	}
 
 	@Override
 	public SetLinkedList<T> intersection(SetLinkedList<T> otherSet) {
-		//TODO Implement your code here
-		throw new UnsupportedOperationException("Not implemented yet!");
+		SetLinkedListImpl<T> aux = new SetLinkedListImpl<>();
+		T[] a = this.toArray();
+		for (T element : a) {
+			if (otherSet.search(element) != null) {
+				aux.insert(element);
+			}
+		}
+
+		T[] b = otherSet.toArray();
+		for (T element : b) {
+			if (this.search(element) != null && aux.search(element) == null) {
+				aux.insert(element);
+			}
+		}
+		return aux;
 	}
 
 	@Override
 	public void concatenate(SetLinkedList<T> otherSet) {
-		//TODO Implement your code here
-		throw new UnsupportedOperationException("Not implemented yet!");
+		T[] a = otherSet.toArray();
+		for (T element : a) {
+			if (this.search(element) == null) {
+				this.insert(element);
+			}
+		}
 	}
 
 }
