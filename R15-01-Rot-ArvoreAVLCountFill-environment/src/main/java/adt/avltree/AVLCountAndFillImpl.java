d2e0e97097fb2e7c701b08009f9d819362abae14
Mateus Santos <mateus.santos@ccc.ufcg.edu.br>
2019-11-07 14:53:36 -0300

    added r15

---
+++ R15-01-Rot-ArvoreAVLCountFill-environment/src/main/java/adt/avltree/AVLCountAndFillImpl.java
@@ -0,0 +1,133 @@
+package adt.avltree;
+
+import java.util.Arrays;
+
+import adt.bst.BSTNode;
+import adt.bt.Util;
+
+public class AVLCountAndFillImpl<T extends Comparable<T>> extends
+		AVLTreeImpl<T> implements AVLCountAndFill<T> {
+
+	private int LLcounter;
+	private int LRcounter;
+	private int RRcounter;
+	private int RLcounter;
+
+	public AVLCountAndFillImpl() {
+		
+	}
+
+	@Override
+	public int LLcount() {
+		return LLcounter;
+	}
+
+	@Override
+	public int LRcount() {
+		return LRcounter;
+	}
+
+	@Override
+	public int RRcount() {
+		return RRcounter;
+	}
+
+	@Override
+	public int RLcount() {
+		return RLcounter;
+	}
+
+	@Override
+	protected void rebalance(BSTNode<T> node) {
+		if (node != null && !node.isEmpty()) {
+			boolean isRoot = false;
+			if (node.getParent().isEmpty()) {
+				isRoot = true;
+			}
+			int balance = this.calculateBalance(node);
+
+			if (balance > 1) {
+				boolean LROcurrence = false;
+				if (super.height((BSTNode<T>) node.getLeft().getRight()) >
+					super.height((BSTNode<T>) node.getLeft().getLeft())) {
+					node.setLeft(Util.leftRotation((BSTNode<T>) node.getLeft()));
+					this.LRcounter++;
+					LROcurrence = true;
+				}
+				if (isRoot) {
+					this.root = Util.rightRotation(node);
+					if (!LROcurrence) {
+						this.LLcounter++;
+					}
+				} else {
+					if (super.isChildPerLeft(node)) {
+						node.getParent().setLeft(Util.rightRotation(node));
+					} else {
+						node.getParent().setRight(Util.rightRotation(node));
+					}
+					if (!LROcurrence) {
+						this.LLcounter++;
+					}
+				}
+
+			} else if (balance < -1) {
+				boolean RLOcurrence = false;
+				if (super.height((BSTNode<T>) node.getRight().getLeft()) > 
+					super.height((BSTNode<T>) node.getRight().getRight())) {
+					node.setRight(Util.rightRotation((BSTNode<T>) node.getRight()));
+					this.RLcounter++;
+					RLOcurrence = true;
+				}
+				if (isRoot) {
+					this.root = Util.leftRotation(node);
+					if (!RLOcurrence) {
+						this.RRcounter++;
+					}
+				} else {
+					if (super.isChildPerLeft(node)) {
+						node.getParent().setLeft(Util.leftRotation(node));
+					} else {
+						node.getParent().setRight(Util.leftRotation(node));
+					}
+					if (!RLOcurrence) {
+						this.RRcounter++;
+					}
+				}
+
+			}
+		}
+	}
+	
+	@Override
+	public void fillWithoutRebalance(T[] array) {
+		if (array != null && array.length >= 1) {
+			Arrays.parallelSort(array);
+			mergeLikeInsertion(array, 0, array.length - 1);
+		}
+	}
+	
+	private void mergeLikeInsertion(T[] array, int leftIndex, int rightIndex) {
+		if (leftIndex <= rightIndex) {
+
+			int middle = (rightIndex + leftIndex) / 2;
+			super.insert(array[middle]);
+
+			Thread thread = new Thread(new Runnable() {
+				@Override
+				public void run() {
+					mergeLikeInsertion(array, leftIndex, middle-1);
+				}
+			});
+
+			Thread thread2 = new Thread(new Runnable() {
+				@Override
+				public void run() {
+					mergeLikeInsertion(array, middle + 1, rightIndex);
+				}
+			});
+
+			thread.start();
+			thread2.start();
+		}
+	}
+}
---
+++ R15-01-Rot-ArvoreAVLCountFill-environment/src/main/java/adt/avltree/AVLTreeImpl.java
@@ -0,0 +1,143 @@
+package adt.avltree;
+
+import adt.bst.BSTImpl;
+import adt.bst.BSTNode;
+import adt.bt.Util;
+
+/**
+ * 
+ * Performs consistency validations within a AVL Tree instance
+ * 
+ * @author Claudio Campelo
+ *
+ * @param <T>
+ */
+public class AVLTreeImpl<T extends Comparable<T>> extends BSTImpl<T> implements AVLTree<T> {
+
+	// AUXILIARY
+	protected int calculateBalance(BSTNode<T> node) {
+		int retorno = 0;
+
+		if (node != null && !node.isEmpty()) {
+			BSTNode<T> nodeLeft = (BSTNode<T>) node.getLeft();
+			BSTNode<T> nodeRight = (BSTNode<T>) node.getRight();
+			retorno = super.height(nodeLeft) - super.height(nodeRight);
+		}
+
+		return retorno;
+	}
+
+	// AUXILIARY
+	protected void rebalance(BSTNode<T> node) {
+		if (node != null && !node.isEmpty()) {
+			boolean isRoot = false;
+			if (node.getParent().isEmpty()) {
+				isRoot = true;
+			}
+			int balance = this.calculateBalance(node);
+
+			if (balance > 1) {
+				if (super.height((BSTNode<T>) node.getLeft().getRight()) > super.height(
+						(BSTNode<T>) node.getLeft().getLeft())) {
+					node.setLeft(Util.leftRotation((BSTNode<T>) node.getLeft()));
+				}
+				if (isRoot) {
+					this.root = Util.rightRotation(node);
+				} else {
+					if (this.isChildPerLeft(node)) {
+						node.getParent().setLeft(Util.rightRotation(node));
+					} else {
+						node.getParent().setRight(Util.rightRotation(node));
+					}
+				}
+
+			} else if (balance < -1) {
+				if (super.height((BSTNode<T>) node.getRight().getLeft()) > super.height(
+						(BSTNode<T>) node.getRight().getRight())) {
+					node.setRight(Util.rightRotation((BSTNode<T>) node.getRight()));
+				}
+				if (isRoot) {
+					this.root = Util.leftRotation(node);
+				} else {
+					if (this.isChildPerLeft(node)) {
+						node.getParent().setLeft(Util.leftRotation(node));
+					} else {
+						node.getParent().setRight(Util.leftRotation(node));
+					}
+				}
+
+			}
+		}
+	}
+
+	// AUXILIARY
+	protected void rebalanceUp(BSTNode<T> node) {
+		if (node != null && !node.isEmpty()) {
+			this.rebalance(node);
+			this.rebalanceUp((BSTNode<T>) node.getParent());
+		}
+	}
+
+	protected boolean isChildPerLeft(BSTNode<T> node) {
+		boolean retorno = false;
+		if (node != null && !node.isEmpty()) {
+			BSTNode<T> parent = (BSTNode<T>) node.getParent();
+			if (parent.getLeft().equals(node)) {
+				retorno = true;
+			}
+		}
+		return retorno;
+	}
+
+	@Override
+	public void insert(T element) {
+		super.insert(element);
+		BSTNode<T> node = this.search(element);
+		this.rebalanceUp(node);
+	}
+
+	@Override
+	public void remove(T element) {
+		BSTNode<T> node = this.search(element);
+		this.remove(node);
+	}
+
+	private void remove(BSTNode<T> node) {
+		if (!node.isEmpty()) {
+			if (node.isLeaf()) {
+				node.setData(null);
+				node.setLeft(null);
+				node.setRight(null);
+				this.rebalanceUp((BSTNode<T>) node.getParent());
+			} else if (!node.getLeft().isEmpty() && node.getRight().isEmpty()
+					|| node.getLeft().isEmpty() && !node.getRight().isEmpty()) {
+				if (!this.root.equals(node)) {
+					if (this.isChildPerLeft(node)) {
+						if (!node.getLeft().isEmpty()) {
+							node.getParent().setLeft(node.getLeft());
+						} else {
+							node.getParent().setLeft(node.getRight());
+						}
+					} else {
+						if (!node.getLeft().isEmpty()) {
+							node.getParent().setRight(node.getLeft());
+						} else {
+							node.getParent().setRight(node.getRight());
+						}
+					}
+				} else {
+					if (!node.getLeft().isEmpty()) {
+						this.root = (BSTNode<T>) node.getLeft();
+					} else {
+						this.root = (BSTNode<T>) node.getRight();
+					}
+				}
+				this.rebalanceUp(node);
+			} else {
+				BSTNode<T> sucessor = super.sucessor(node.getData());
+				node.setData((T) sucessor.getData());
+				this.remove(sucessor);
+			}
+		}
+	}
+}
